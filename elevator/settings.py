689f8520366800becd0b74e13f8efbb82b653456
narae0409 <58824745+narae0409@users.noreply.github.com>
2020-09-03 19:58:46 +0900

    Update settings.py

--- elevator/settings.py
+++ elevator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o#ehs72hlpq6e^@=#4f%dnnp($&qsravj**t59@p+ofkkm9h2v'
+SECRET_KEY = 'Hidden'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
