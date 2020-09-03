6f85d40b82a8f0792f91b9aba6b15bb5d152aeb5
narae0409 <58824745+narae0409@users.noreply.github.com>
2020-09-03 19:56:50 +0900

    Update settings.py

--- elevator/settings.py
+++ elevator/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # mysql 엔진 설정 
         'NAME':'data', # 데이터베이스 이름 
         'USER':'root', # 데이터베이스 연결시 사용할 유저 이름 
-        'PASSWORD':'tmrk8521', # 유저 패스워드 
+        'PASSWORD':'password', # 유저 패스워드 
         'HOST':'localhost', 
         'PORT':'' 
     } 
