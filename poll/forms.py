e8509f544aee974d8a5b669879be31b38a706911
narae0409 <tkdgn821@naver.com>
2020-12-24 00:32:27 +0900

    update web service

--- poll/forms.py
+++ poll/forms.py
@@ -9,3 +9,6 @@ class ElvtAddress(forms.Form):
 class ElvtLogin(forms.Form):
     ID = forms.CharField(label="ID")
     password = forms.CharField(label="Password", widget=forms.PasswordInput())
+
+class ElvtAddresList(forms.Form):
+    address = forms.CharField()
